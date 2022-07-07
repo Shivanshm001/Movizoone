65075fbffd261f7ce8dbbeae02c199eb1facf4cd
Shivanshm001 <Shivanshmishra084@gmail.com>
2022-07-08 00:53:46 +0530

    Basic structure complete,excluding responsiveness&scrollbars

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ app.use("/public",express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.get("/",(req,res)=>{[
+app.set('view engine','ejs');
+
+
+app.get("/",(req,res)=>{
+    // res.render("index")
     res.sendFile(path.join(__dirname,"index.html"))
-]})
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,(err)=>{
     if(err) console.log(err);
